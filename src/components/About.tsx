4700284245e1d21111ebae001899fbaf8538ef37
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-10 07:05:51 +0530

    About styling fixes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,15 @@
 function About() {
   return (
     <div className="w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black ">
-      <h1 className=" font-neue text-[4vw] leading-[3.5vw] tracking-tighter">
+      <h1 className=" font-neue text-[4vw] leading-[3.5vw] tracking-tight">
         Ochi is a strategic partner for fast-growing tech businesses that need
         to raise funds, sell products, explain complex ideas, and hire great
         people.
       </h1>
       <div className="flex gap-5 w-full border-t-[2px] pt-10 mt-20 border-[#a1b562] ">
         <div className="w-1/2 ">
-            <h1 className="text-7xl tracking-tighter">Our approach:</h1>
-            <button className="flex gap-10 uppercase text-2xl items-center px-10 py-6 bg-zinc-900 mt-10 rounded-full text-white">Read More
+            <h1 className="text-6xl tracking-tighter">Our approach:</h1>
+            <button className="flex gap-10 uppercase text-[1vw] items-center tracking-wide px-8 py-5 bg-zinc-900 mt-10 rounded-full text-white">Read More
                 <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
             </button>
         </div>
