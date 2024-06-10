a6f607d36b527641f2f671c7c70ca791446bba9f
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-10 12:00:55 +0530

    Marquee animation

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,14 +1,22 @@
+import { motion } from "framer-motion";
 import { FaArrowUpLong } from "react-icons/fa6";
 
 function LandingPage() {
   return (
-    <div className="h-screen w-full bg-zinc-900 pt-1">
+    <div data-scroll data-scroll-section data-scroll-speed="-.3" className="h-screen w-full bg-zinc-900 pt-1">
       <div className="textstructure mt-52 px-20 ">
         {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
           return (
             <div className="masker " key={index}>
               <div className="flex w-fit items-center pb-[1.2vh] overflow-hidden">
-                {index ===1 && (<div className="w-[9vw] mr-5 mt-[1.8vh] bg-red-500 h-[5.7vw] rounded-md "></div>)}
+                {index === 1 && (
+                  <motion.div
+                    initial={{ width: 0 }}
+                    animate={{ width: "9vw" }}
+                    transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
+                    className="w-[9vw] mt-[1.8vh] mr-3 bg-red-500 h-[5.7vw] bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] bg-cover bg-no-repeat rounded-md "
+                  ></motion.div>
+                )}
                 <h1 className=" font-founder leading-[7vw] uppercase text-[9vw] ">
                   {item}
                 </h1>
--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,12 +1,28 @@
+import { motion } from "framer-motion";
+
 function Marquee() {
   return (
-    <div className="w-full py-20 rounded-tl-3xl  rounded-tr-3xl bg-[#004d43]">
-        <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap">
-            <h1 className="text-[24vw] leading-none font-founder  uppercase -mt-5 ">we are ochi</h1>
-            <h1 className="text-[24vw] leading-none font-founder  uppercase -mt-5 ">we are ochi</h1>
-        </div>
+    <div data-scroll data-scroll-section data-scroll-speed=".1" className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex  overflow-hidden whitespace-nowrap">
+        <motion.h1
+          initial={{ x: "0" }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[24vw] pr-20 leading-none font-founder  uppercase -mt-5 "
+        >
+          we are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: "0" }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[24vw] pr-20 leading-none font-founder  uppercase -mt-5 "
+        >
+          we are ochi
+        </motion.h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Marquee
+export default Marquee;
